8e84194fa1018505e539a58d6464c68edaeaeb03
Richard George <rcgeorge23@gmail.com>
2018-09-08 11:56:58 +0100

    some more test scenarios

--- src/it/groovy/uk/co/novinet/web/IndividualIT.groovy
+++ src/it/groovy/uk/co/novinet/web/IndividualIT.groovy
@@ -19,12 +19,6 @@ class IndividualIT extends GebSpec {
     //Server side validation
     //Client side validation combinations
     //Re-populating form after submission when error occurs
-    //Add a message to the front end form saying that amount is inclusive of VAT for contribution agreements
-    //Add the contribution agreement attachment (with RH's signature) - DONE
-    //Check that lcag ffc contributor group is added to profile of existing member - DONE
-    //Check that lcag ffc contributor group is added to profile of newly created member - DONE
-    //Donation confirmation email DOES NOT have an invoice attachment - DONE
-    //Contribution agreement email DOES have an invoice attachment - DONE
 
     def setup() {
         TestUtils.setupDatabaseSchema()
@@ -99,7 +93,7 @@ class IndividualIT extends GebSpec {
             waitFor { getEmails("user1@something.com", "Inbox").size() == 1 }
             waitFor { getEmails("user1@something.com", "Inbox").get(0).content.contains("Dear Test Name1, Thank you for your contribution of £10.00 towards the Loan Charge Action Group litigation fund. Your payment reference is LCAGFFC90001. Many thanks, LCAG FFC Team") }
             waitFor { getUserRows().get(0).getGroup() == "8" }
-            waitFor { getUserRows().get(0).getAdditionalGroups() == "9" }
+            assert isBlank(getUserRows().get(0).getAdditionalGroups())
             GebTestUtils.verifyNoAttachments("user1@something.com")
 
         when: "i navigate to the invoice page"
@@ -330,6 +324,134 @@ class IndividualIT extends GebSpec {
 
         then:
             verifyInvoice(browser, "LCAGFFC90001", new Date(), "Card", "Harry Generous", "harry@generous.com", "Contribution Agreement", "£1,666.67", "20%", "£333.33", "£2,000.00", "1234567890")
+    }
+
+    def "i can complete the payment flow for £2000 but choose donation instead of contribution agreement for a new lcag applicant"() {
+        given:
+            sleep(3000) //wait for member cache to be refreshed
+            getUserRows().size() == 0
+            GebTestUtils.driveToPaymentType(browser, "2000", PaymentType.NEW_LCAG_MEMBER, ContributionType.DONATION)
+
+        when: "i enter valid lcag username value and payment details and click pay now"
+            assert addressLine1Input.displayed == false
+            assert addressLine2Input.displayed == false
+            assert cityInput.displayed == false
+            assert postalCodeInput.displayed == false
+            assert countryInput.displayed == false
+            assert contributionAgreementInfoSection.displayed == true
+            assert donationInfoSection.displayed == false
+
+            firstNameInput = "Harry"
+            lastNameInput = "Generous"
+            emailAddressInput = "harry@generous.com"
+            enterCardDetails(browser, AUTHORIZED_CARD, "0222", "111", "33333")
+            payNowButton.click()
+            waitFor { at ThankYouPage }
+            waitFor { getEmails("harry@generous.com", "Inbox").size() == 1 }
+            String emailContent = getEmails("harry@generous.com", "Inbox").get(0).content
+
+        then: "i land on the thank you page and i receive a confirmation email"
+            waitFor { at ThankYouPage }
+            waitFor { paymentReference.text() == "LCAGFFC90001" }
+            waitFor { getUserRows().size() == 1 }
+            waitFor { getUserRows().get(0).emailAddress == "harry@generous.com" }
+            waitFor { getUserRows().get(0).name == "Harry Generous" }
+            waitFor { getEmails("harry@generous.com", "Inbox").size() == 1 }
+            waitFor { emailContent.contains("Dear Harry Generous, Thank you for your contribution of £2,000.00 towards the Loan Charge Action Group litigation fund. Your payment reference is LCAGFFC90001. You have indicated that you would like to join LCAG and be kept up to date with the latest developments regarding the 2019 Loan Charge legal challenge. We have already set up a forum user account for you: Username: harry Temporary password:") }
+            waitFor { emailContent.contains("Temporary password: 2019l0anCharg3") || emailContent.contains("Temporary password: lc4g2019Ch4rg3") || emailContent.contains("Temporary password: hm7cL04nch4rGe") || emailContent.contains("Temporary password: ch4l1Eng3Hm7C") }
+            waitFor { emailContent.contains("You can access the forum from here: https://forum.hmrcloancharge.info/ Initially your ability to interact on the forum will be limited to the ‘Guest’ and ‘Welcome’ areas. This restriction will be lifted once we have verified your identity. In order to verify your identity we need to collect some additional information. If you are happy to proceed, please complete the LCAG membership form and we will get back to you as soon as we can: https://membership.hmrcloancharge.info?token=${getUserRows().get(0).membershipToken} Many thanks, LCAG FFC Team") }
+            verifyNoAttachments("harry@generous.com")
+            waitFor { getUserRows().get(0).getGroup() == "8" }
+            assert isBlank(getUserRows().get(0).getAdditionalGroups())
+            waitFor { getPaymentRows().size() == 1 }
+            waitFor { getPaymentRows().get(0).id == 1L }
+            waitFor { getPaymentRows().get(0).userId == 1L }
+            waitFor { getPaymentRows().get(0).firstName == "Harry" }
+            waitFor { getPaymentRows().get(0).lastName == "Generous" }
+            waitFor { getPaymentRows().get(0).emailAddress == "harry@generous.com" }
+            waitFor { getPaymentRows().get(0).reference == "LCAGFFC90001" }
+            waitFor { getPaymentRows().get(0).contributionType == ContributionType.DONATION }
+            waitFor { getPaymentRows().get(0).paymentMethod == "Card" }
+
+        when: "i navigate to the invoice page"
+            go driver.currentUrl.replace("thankYou", "invoice")
+            waitFor { at InvoicePage }
+
+        then:
+            verifyNoVatNumberInvoice(browser, "LCAGFFC90001", new Date(), "Card", "Harry Generous", "harry@generous.com", "Donation", "£2,000.00")
+
+        when: "i set the vat number and refresh the page"
+            runSqlUpdate("update i7b0_ffc_contributions set vat_number = '1234567890' where `reference` = 'LCAGFFC90001'")
+            driver.navigate().refresh()
+            waitFor { at InvoicePage }
+
+        then:
+            verifyInvoice(browser, "LCAGFFC90001", new Date(), "Card", "Harry Generous", "harry@generous.com", "Donation", "£2,000.00", "0%", "£0.00", "£2,000.00", "1234567890")
+    }
+
+    def "i can complete the payment flow for £2000 but choose donation instead of contribution agreement for an existing lcag member"() {
+        given:
+            insertUser(1, "testuser1", "user1@something.com", "Test Name1", 8, "1234_1", "claim_1")
+            sleep(3000) //wait for member cache to be refreshed
+            getUserRows().size() == 0
+            GebTestUtils.driveToPaymentType(browser, "2000", PaymentType.EXISTING_LCAG_MEMBER, ContributionType.DONATION)
+
+        when: "i enter valid lcag username value and payment details and click pay now"
+            assert firstNameInput.displayed == false
+            assert lastNameInput.displayed == false
+            assert emailAddressInput.displayed == false
+            assert addressLine1Input.displayed == false
+            assert addressLine2Input.displayed == false
+            assert cityInput.displayed == false
+            assert postalCodeInput.displayed == false
+            assert countryInput.displayed == false
+            assert contributionAgreementInfoSection.displayed == true
+            assert donationInfoSection.displayed == false
+
+            username = "testuser1"
+
+            enterCardDetails(browser, AUTHORIZED_CARD, "0222", "111", "33333")
+
+            payNowButton.click()
+
+            waitFor { getEmails("user1@something.com", "Inbox").size() == 1 }
+            String emailContent = getEmails("user1@something.com", "Inbox").get(0).content
+
+        then: "i land on the thank you page and i receive a confirmation email"
+            waitFor { at ThankYouPage }
+            waitFor { paymentReference.text() == "LCAGFFC90001" }
+            waitFor { getUserRows().size() == 1 }
+            waitFor { getUserRows().get(0).emailAddress == "user1@something.com" }
+            waitFor { getUserRows().get(0).name == "Test Name1" }
+            waitFor { getEmails("user1@something.com", "Inbox").size() == 1 }
+            waitFor { emailContent.contains("Dear Test Name1, Thank you for your contribution of £2,000.00 towards the Loan Charge Action Group litigation fund. Your payment reference is LCAGFFC90001. Many thanks, LCAG FFC Team") }
+            verifyNoAttachments("user1@something.com")
+            waitFor { getUserRows().get(0).getGroup() == "8" }
+            assert isBlank(getUserRows().get(0).getAdditionalGroups())
+            waitFor { getPaymentRows().size() == 1 }
+            waitFor { getPaymentRows().get(0).id == 1L }
+            waitFor { getPaymentRows().get(0).userId == 1L }
+            waitFor { getPaymentRows().get(0).firstName == "Test" }
+            waitFor { getPaymentRows().get(0).lastName == "Name1" }
+            waitFor { getPaymentRows().get(0).emailAddress == "user1@something.com" }
+            waitFor { getPaymentRows().get(0).reference == "LCAGFFC90001" }
+            waitFor { getPaymentRows().get(0).contributionType == ContributionType.DONATION }
+            waitFor { getPaymentRows().get(0).paymentMethod == "Card" }
+
+        when: "i navigate to the invoice page"
+            go driver.currentUrl.replace("thankYou", "invoice")
+            waitFor { at InvoicePage }
+
+        then:
+            verifyNoVatNumberInvoice(browser, "LCAGFFC90001", new Date(), "Card", "Test Name1", "user1@something.com", "Donation", "£2,000.00")
+
+        when: "i set the vat number and refresh the page"
+            runSqlUpdate("update i7b0_ffc_contributions set vat_number = '1234567890' where `reference` = 'LCAGFFC90001'")
+            driver.navigate().refresh()
+            waitFor { at InvoicePage }
+
+        then:
+            verifyInvoice(browser, "LCAGFFC90001", new Date(), "Card", "Test Name1", "user1@something.com", "Donation", "£2,000.00", "0%", "£0.00", "£2,000.00", "1234567890")
     }
 
 }
