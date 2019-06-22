e02c66adf98eb63ea80d862c32fc7d0f72d74f0a
stratos <stratos-team@bskyb.com>
2019-06-22 08:39:15 +0100

    [RG] Warmed up lcag ffc. Tests now pass again with latest spring boot and stripe libs

--- src/it/groovy/GebConfig.groovy
+++ src/it/groovy/GebConfig.groovy
@@ -1,4 +1,42 @@
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeDriverService
+import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.os.ExecutableFinder
 
-System.setProperty("webdriver.chrome.driver", "/Users/rcgeorge23/Documents/workspace/lcagffc/browser-drivers/chromedriver");
-driver = { new ChromeDriver() }
+import static org.apache.commons.lang3.SystemUtils.*
+
+File findDriverExecutable() {
+    def defaultExecutable = new ExecutableFinder().find("chromedriver")
+    if (defaultExecutable) {
+        new File(defaultExecutable)
+    } else {
+        new File("drivers").listFiles().findAll {
+            !it.name.endsWith(".version")
+        }.find {
+            if (IS_OS_LINUX) {
+                it.name.contains("linux")
+            } else if (IS_OS_MAC) {
+                it.name.contains("mac")
+            } else if (IS_OS_WINDOWS) {
+                it.name.contains("windows")
+            }
+        }
+    }
+}
+
+driver = {
+    ChromeDriverService.Builder serviceBuilder = new ChromeDriverService.Builder()
+            .usingAnyFreePort()
+            .usingDriverExecutable(findDriverExecutable())
+
+    if (System.getProperty("headlessChrome", "false") == "true") {
+        ChromeOptions chromeOptions = new ChromeOptions()
+        chromeOptions.addArguments("--headless")
+        chromeOptions.addArguments("--no-sandbox")
+
+        return new ChromeDriver(serviceBuilder.build(), chromeOptions)
+    } else {
+        return new ChromeDriver(serviceBuilder.build())
+    }
+
+}
